38acbd55b44970be164150661e21b1f936e57944
Darren Koepp <darren.koepp@gree.co.jp>
2014-11-02 16:53:19 -0800

    updated phonegap version

--- platforms/android/src/com/onetonghost/ballcard/CordovaApp.java
+++ platforms/android/src/com/onetonghost/ballcard/CordovaApp.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package com.onetonghost.scorecard;
+package com.onetonghost.ballcard;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
--- platforms/ios/ballcard/main.m
+++ platforms/ios/ballcard/main.m
@@ -17,10 +17,19 @@
  under the License.
  */
 //
-// Prefix header for all source files of the 'scorecard' target in the 'scorecard' project
+//  main.m
+//  ballcard
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
 //
 
-#ifdef __OBJC__
-    #import <Foundation/Foundation.h>
-    #import <UIKit/UIKit.h>
-#endif
+#import <UIKit/UIKit.h>
+
+int main(int argc, char* argv[])
+{
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
